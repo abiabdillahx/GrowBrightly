54c1f3ec2f37174f91e8c15671060e0df9d84f3e
Abi <83850574+abiabdillahx@users.noreply.github.com>
2025-05-10 17:20:59 +0700

    chore: page setup
    belajar next

---
+++ src/app/about/page.js
@@ -0,0 +1,17 @@
+// app/about/page.js
+
+import Head from "next/head";
+
+export default function AboutPage() {
+  return (
+    <><Head>
+          <title>About Us - GrowthBrightly</title>
+      </Head>
+      
+      <div>
+              <h1>About Us</h1>
+              <p>Welcome to the About page!</p>
+              <p>This is a simple page created with Next.js.</p>
+          </div></>
+  );
+}
